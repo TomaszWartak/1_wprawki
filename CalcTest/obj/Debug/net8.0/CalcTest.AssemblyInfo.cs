0b702f1dc2007b3a52de962961d28f48c69a8573
tomaszwartak <tomasz.wartak@gmail.com>
2024-04-02 10:45:54 +0200

    Porządki

--- CalcTest/obj/Debug/net8.0/CalcTest.AssemblyInfo.cs
+++ CalcTest/obj/Debug/net8.0/CalcTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalcTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99387104802c07267296f0d33c57cd053119521d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalcTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalcTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
